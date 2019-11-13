19e872b2a36d961b10d9235339c3f1efdecc147a
Arthur Michel <arthur.michel1@gmail.com>
2019-11-13 10:55:39 +0100

    wikiminus

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+require 'faker'
+include Faker
+
+10.times do
+  Article.create(
+    title: Faker::FunnyName.name,
+    content: Faker::Demographic.race
+  )
+  puts 'created Articles'
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +17,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
